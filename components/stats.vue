bb7224dc1f0a1dfc7710ec427170e0cf61aade28
godwill.codes <iamgodwillb@gmail.com>
2023-01-02 03:12:14 +0300

    more components

--- components/stats.vue
+++ components/stats.vue
@@ -1,25 +1,30 @@
 <template>
-    <div class="bg-indigo-800">
-      <div class="mx-auto max-w-7xl py-12 px-4 sm:py-16 sm:px-6 lg:px-8 lg:py-20">
-        <div class="mx-auto max-w-4xl text-center">
-          <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Trusted by developers from over 80 planets</h2>
-          <p class="mt-3 text-xl text-indigo-200 sm:mt-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus repellat laudantium.</p>
+    <div class="bg-gray-900">
+      <div class="mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        <div class="lg:mx-auto lg:max-w-2xl lg:text-center">
+          <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Frequently asked questions</h2>
+          <p class="mt-4 text-gray-400">Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla nec. Urna, sed a lectus elementum blandit et.</p>
         </div>
-        <dl class="mt-10 text-center sm:mx-auto sm:grid sm:max-w-3xl sm:grid-cols-3 sm:gap-8">
-          <div class="flex flex-col">
-            <dt class="order-2 mt-2 text-lg font-medium leading-6 text-indigo-200">Pepperoni</dt>
-            <dd class="order-1 text-5xl font-bold tracking-tight text-white">100%</dd>
-          </div>
-          <div class="mt-10 flex flex-col sm:mt-0">
-            <dt class="order-2 mt-2 text-lg font-medium leading-6 text-indigo-200">Delivery</dt>
-            <dd class="order-1 text-5xl font-bold tracking-tight text-white">24/7</dd>
-          </div>
-          <div class="mt-10 flex flex-col sm:mt-0">
-            <dt class="order-2 mt-2 text-lg font-medium leading-6 text-indigo-200">Calories</dt>
-            <dd class="order-1 text-5xl font-bold tracking-tight text-white">100k+</dd>
-          </div>
-        </dl>
+        <div class="mt-20">
+          <dl class="space-y-10 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:gap-y-10 lg:space-y-0">
+            <div v-for="faq in faqs" :key="faq.id">
+              <dt class="font-semibold text-white">{{ faq.question }}</dt>
+              <dd class="mt-3 text-gray-400">{{ faq.answer }}</dd>
+            </div>
+          </dl>
+        </div>
       </div>
     </div>
   </template>
   
+  <script setup>
+  const faqs = [
+    {
+      id: 1,
+      question: "What's the best thing about Switzerland?",
+      answer:
+        "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+    },
+    // More questions...
+  ]
+  </script>
